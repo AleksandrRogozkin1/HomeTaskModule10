47f1a969f6fc27411974a520faf2c79f9caae510
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-02-28 00:25:19 +0200

    Initial commit

--- src/main/java/Module10/Main.java
+++ src/main/java/Module10/Main.java
@@ -11,18 +11,18 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        FileReader fr =new FileReader("C:\\IdeaProjects\\HomeTaskModule10\\untitled\\src\\main\\java\\Module10\\PhoneNumber\\file.txt");
+        FileReader fr =new FileReader("src\\main\\java\\Module10\\PhoneNumber\\file.txt");
         Queue<String> queue= fr.reading();
         for (String result:queue) {
             PhoneNumberValidator.validator(result);
         }
         queue.clear();
 
-        WordFrequencyCounter wfc=new WordFrequencyCounter("C:\\IdeaProjects\\HomeTaskModule10\\untitled\\src\\main\\java\\Module10\\FrequencyCounter\\words.txt");
+        WordFrequencyCounter wfc=new WordFrequencyCounter("src\\main\\java\\Module10\\FrequencyCounter\\words.txt");
         wfc.wordsCounter();
 
         TxtToJSON txtToJSON=new TxtToJSON();
-        System.out.println(txtToJSON.saveJson("C:\\IdeaProjects\\HomeTaskModule10\\untitled\\src\\main\\java\\Module10\\TextJSON\\file.txt","C:\\IdeaProjects\\HomeTaskModule10\\untitled\\src\\main\\java\\Module10\\TextJSON\\user.json"));
+        System.out.println(txtToJSON.saveJson("src\\main\\java\\Module10\\TextJSON\\file.txt","src\\main\\java\\Module10\\TextJSON\\user.json"));
 
 
 
